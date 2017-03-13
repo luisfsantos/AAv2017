ec194afcb85bdbf141ec835fed9a403669fcd807
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2017-03-13 11:25:03 +0000

    complete command closes #1, add naive string matching

--- sma.c
+++ sma.c
@@ -25,23 +25,28 @@
 #define REDUCTION 4
 #endif
 
-int size[2] = {1,1};
+int content_size[2] = {0,0};
 int space[2] = {1,1};
-int location[2] = {0,0};
+int buffer_size[2] = {1,1};
 char *text;
 char *pattern;
 
 void test(void** func);
 void run();
-char * update_buffer(char * buffer, int * size);
+char * update_buffer(char * buffer, int * size, int * content_size);
 int execute_command(char command);
 int calc_diff(int index, int space);
 void delete_all();
 
+/* ALGORITHIMS */
+void naive_algorithim(char * text, int text_size, char * pattern, int pattern_size);
+void kmp_algorithim(char * text, int text_size, char * pattern, int pattern_size);
+void bm_algorithim(char * text, int text_size, char * pattern, int pattern_size);
+
 int main(int argc, char const *argv[])
 {
-	text = (char *) malloc(size[TEXT_IDX] * sizeof(char));
-	pattern = (char *) malloc(size[PATTERN_IDX] * sizeof(char));
+	text = (char *) malloc(buffer_size[TEXT_IDX] * sizeof(char));
+	pattern = (char *) malloc(buffer_size[PATTERN_IDX] * sizeof(char));
 
 	if (argc > 0 && strncmp(TEST, argv[0], TEST_SIZE)) {
 		/*test(run());*/
@@ -69,47 +74,99 @@ void run() {
 	char command;
 	do {
 		command = getc(stdin);
-		printf("%s\n", text);
-		//printf("%c\n", command);
-		//printf("%c\n", getc(stdin));/* ignore white space after the command */
 	} while (execute_command(command));
 }
 
+
+
+/*******************************************************************
+* DESCRIPTION :     Executes the command that is given
+*
+* INPUTS :
+*       PARAMETERS:
+*           char      command               command to execute
+*       GLOBALS :
+*           char * 	  text 					array containing the text
+*			char *    pattern 				array containing the pattern to match
+* OUTPUTS :
+*       PARAMETERS:
+*           int    	  run             		whether the command was an exit or nor.
+*       GLOBALS :
+*            None
+* PROCESS :
+*
+* NOTES :           
+*/
 int execute_command(char command) {
 	int run = 1;
 	switch (command) {
 		case TEXT_UPDATE:
-			text = update_buffer(text, &size[TEXT_IDX]);
+			text = update_buffer(text, &buffer_size[TEXT_IDX], &content_size[TEXT_IDX]);
 			break; 
 		case NAIVE:
-			pattern = update_buffer(pattern, &size[PATTERN_IDX]);
-			/*UPDATE THE TEXT;*/
+			pattern = update_buffer(pattern, &buffer_size[PATTERN_IDX], &content_size[PATTERN_IDX]);
+			naive_algorithim(text, content_size[TEXT_IDX], pattern, content_size[PATTERN_IDX]);
 			break;
 		case KMP:
-			pattern = update_buffer(pattern, &size[PATTERN_IDX]);
-			/*UPDATE THE TEXT;*/
+			pattern = update_buffer(pattern, &buffer_size[PATTERN_IDX], &content_size[PATTERN_IDX]);
+			kmp_algorithim(text, content_size[TEXT_IDX], pattern, content_size[PATTERN_IDX]);
 			break;
 		case BM:
-			pattern = update_buffer(pattern, &size[PATTERN_IDX]);
-			/*UPDATE THE TEXT;*/
+			pattern = update_buffer(pattern, &buffer_size[PATTERN_IDX], &content_size[PATTERN_IDX]);
+			bm_algorithim(text, content_size[TEXT_IDX], pattern, content_size[PATTERN_IDX]);
 			break;
 		case EXIT:
 			run = 0;
 			delete_all();
 			break;
 		default:
-			printf("%s\n", "Not a command, RTFM!");
+			printf("%s\n", "Not a command, please use the pattern (C \"space\" TEXT):");
+			printf("\t%s\n", " - T to update the text.");
+			printf("\t%s\n", " - N to run the pattern against the text with a naive implementation.");
+			printf("\t%s\n", " - K to run the pattern against the text with a KMP implementation.");
+			printf("\t%s\n", " - B to run the pattern against the text with a Boyer Moore implementation.");
+			printf("\t%s\n", " - X to exit safely freeing all memory.");
+			while (getc(stdin) != '\n');
 			break;
 	}
 	return run;
 }
 
 
-/* TODO decide if return size or make int size -> int* size*/
-char * update_buffer(char * buffer, int * size) {
+/*******************************************************************
+* DESCRIPTION :     Update a dynamic buffer with the DYNAMIC_MEM
+					modifier (at 2 we have doubling)
+*
+* INPUTS :
+*       PARAMETERS:
+*           char*     buffer                pointer to the buffer
+*			int*	  size 					pointer to the actual size of the buffer
+*       GLOBALS :
+*           int 	  GROWTH				dynamic memory growth on full
+*			int 	  REDUCTION             dynamic memory reduction on lots of free space
+* OUTPUTS :
+*       PARAMETERS:
+*           char    * temp_buff             new pointer to modified array
+*       GLOBALS :
+*            None
+* PROCESS :
+*                   [1]  get one char (should be a space " ")
+*                   [2]  Do
+*                   [3]    Get character
+*					[4]	   if no space
+*								apply dynamic memory growth
+*					[5]	   place char 
+*                   [6]  Until newline
+*					[7]  if too much space
+*							reduce size
+*
+* NOTES :           
+*/
+char * update_buffer(char * buffer, int * size, int * content_size) {
 	int carryOn = 1;
 	int space = *size;
 	int index = 0;
+	char * temp_buff = buffer;
 	getc(stdin); /* ignore white space after the command */
 	do {
 
@@ -124,19 +181,21 @@ char * update_buffer(char * buffer, int * size) {
 		}
 
 		/*Grow and overwrite*/
-		if(space > 0) {
-			printf("I have space (%d) so I will place __ %c __\n", space, c);
-			buffer[index] = c;
-			index++;
-			space--;
-		} else {
-			space+=*size-1;
+		if(space == 0) {
+			space+=*size;
 			*size = *size*GROWTH;
-			printf("I have no space so I will double to %d and now I have %d free spaces and am placing __ %c __\n", *size, space, c);
-			buffer = (char *) realloc(buffer, *size*sizeof(char));
-			buffer[index] = c;
-			index++;
+			printf("I have no space so I will double to %d and now I have %d free spaces \n", *size, space);
+			temp_buff = (char *) realloc(buffer, *size*sizeof(char));
+			if (temp_buff != NULL) {
+				buffer = temp_buff;
+			} else {
+				exit(-1);
+			}
 		}
+		printf("I have space (%d) so I will place __ %c __\n", space, c);
+		buffer[index] = c;
+		index++;
+		space--;
 
 		
 
@@ -147,14 +206,18 @@ char * update_buffer(char * buffer, int * size) {
 		*size = *size/calc_diff(index, space);
 		space=*size-index-1;
 		printf("I have too much space so I will reduce to %d and now I have %d free spaces \n", *size, space);
-		buffer = (char *) realloc(buffer, *size*sizeof(char));
-		/*buffer[index] = c;
-		index++;*/
+		temp_buff = (char *) realloc(buffer, *size*sizeof(char));
+		if (temp_buff != NULL) {
+			buffer = temp_buff;
+		} else {
+			exit(-1);
+		}
 	}
 
-	//printf("%d\n", *size);
-	return buffer;
+	*content_size = index-1;
+	return temp_buff;
 }
+
 
 int calc_diff(int index, int space) {
 	int diff = GROWTH;
@@ -167,4 +230,33 @@ int calc_diff(int index, int space) {
 void delete_all() {
 	free(text);
 	free(pattern);
+}
+
+void naive_algorithim(char * text, int text_size, char * pattern, int pattern_size) {
+	int index = 0;
+	int pattern_index = 0;
+	int text_index = 0;
+	int length = text_size-pattern_size;
+	while (index <= length) {
+		while(text[text_index] == pattern[pattern_index]) {
+			if (pattern_index == pattern_size-1) {
+				printf("%d ", index);
+				break;
+			}
+			text_index++;
+			pattern_index++;
+		}
+		index++;
+		text_index = index;
+		pattern_index = 0;
+	}
+	printf("\n");
+}
+
+void kmp_algorithim(char * text, int text_size, char * pattern, int pattern_size) {
+	
+}
+
+void bm_algorithim(char * text, int text_size, char * pattern, int pattern_size) {
+	
 }
